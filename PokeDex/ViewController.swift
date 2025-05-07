a63136473ed4414a465553e7b33208de9199d5c2
“Deepak” <deepak.ghadi@idsnext.com>
2025-05-07 20:09:42 +0530

    Initial Commit

---
+++ PokeDex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Deepak Ghadi on 07/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PokeDexTests/PokeDexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexTests.swift
+//  PokeDexTests
+//
+//  Created by Deepak Ghadi on 07/05/25.
+//
+
+import Testing
+@testable import PokeDex
+
+struct PokeDexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
